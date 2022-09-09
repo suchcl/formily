a004617c23fdef3a9e8b182fecb5c80623cd1769
suchcl <ya5969@163.com>
2022-09-09 12:47:10 +0800

    card

--- src/pages/card/index.tsx
+++ src/pages/card/index.tsx
@@ -1,10 +1,29 @@
-import { Card } from 'antd';
+import { Card, Input, Select } from 'antd';
+const { Option } = Select;
+import './card.less';
 const CardDemo = () => {
+  const handleOrgChange = () => {
+    console.log('组织变更了');
+  };
   return (
     <>
       <Card
+        className="custom"
         title="自定义标题"
-        extra={<a href="#">More</a>}
+        extra={
+          <>
+            <div className="updatetime">09-09 18:00 数据已更新</div>
+            <Input
+              addonBefore="https://"
+              addonAfter=".com"
+              defaultValue="163"
+            />
+            <Select onChange={handleOrgChange}>
+              <Option value="1">北京公司</Option>
+              <Option value="2">上海公司</Option>
+            </Select>
+          </>
+        }
         style={{ color: '#f20' }}
       >
         <p>我是卡片内容</p>
