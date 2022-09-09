be3b0489851e56797699dbe3eab8a96b732c26d1
suchcl <ya5969@163.com>
2022-09-09 17:54:26 +0800

    图表demo

--- src/pages/detail/detail.tsx
+++ src/pages/detail/detail.tsx
@@ -1,7 +1,41 @@
+import { Bar } from '@ant-design/charts';
 export default () => {
+  const data = [
+    {
+      action: '浏览网站',
+      pv: 50000,
+    },
+    {
+      action: '放入购物车',
+      pv: 35000,
+    },
+    {
+      action: '生成订单',
+      pv: 25000,
+    },
+    {
+      action: '支付订单',
+      pv: 15000,
+    },
+    {
+      action: '完成交易',
+      pv: 8500,
+    },
+  ];
+  const config = {
+    data,
+    width: 400,
+    xField: 'pv',
+    yField: 'action',
+    axis: {
+      position: 'left',
+    },
+    conversionTag: {},
+  };
   return (
     <>
       <h3>详情页</h3>
+      <Bar {...config} />
     </>
   );
 };
--- src/pages/formily/index.tsx
+++ src/pages/formily/index.tsx
@@ -3,146 +3,168 @@ import { createForm } from '@formily/core';
 import 'antd/dist/antd.css';
 import { message, Modal } from 'antd';
 import { NewMixinParser, Form } from 'model-parser'; //需按包
+import './index.less';
 
 const components = {};
 const schema = {
-  form: {
-    labelCol: 6,
-    wrapperCol: 12,
-    selectEffects: [
-      'wfku4nfegcf.y2flsqm782j.time',
-      'wfku4nfegcf.7woqjms6la5.loanMark',
-    ],
-  },
+  form: { labelCol: 6, wrapperCol: 12 },
   schema: {
     type: 'object',
     properties: {
-      wfku4nfegcf: {
+      n672o6hxoxp: {
         type: 'void',
         'x-component': 'FormGrid',
         'x-validator': [],
         'x-component-props': {},
-        'x-designable-id': 'wfku4nfegcf',
-        'x-index': 0,
+        'x-designable-id': 'n672o6hxoxp',
         properties: {
-          y2flsqm782j: {
+          kznkt91o3ap: {
             type: 'void',
             'x-component': 'FormGrid.GridColumn',
             'x-validator': [],
             'x-component-props': {},
-            'x-designable-id': 'y2flsqm782j',
-            'x-index': 0,
+            'x-designable-id': 'kznkt91o3ap',
             properties: {
-              time: {
-                type: 'string[]',
-                title: '交易日期',
-                'x-decorator': 'FormItem',
-                'x-component': 'DatePicker.RangePicker',
-                'x-validator': [],
-                'x-component-props': {},
-                'x-decorator-props': {
-                  labelCol: 4,
-                  wrapperCol: 16,
-                  labelAlign: 'right',
-                  addonBefore: '',
-                },
-                name: 'time',
-                'x-designable-id': '7fast772ovo',
-                'x-index': 0,
-              },
-            },
-          },
-          '7woqjms6la5': {
-            type: 'void',
-            'x-component': 'FormGrid.GridColumn',
-            'x-validator': [],
-            'x-component-props': {},
-            'x-designable-id': '7woqjms6la5',
-            'x-index': 1,
-            properties: {
-              loanMark: {
-                title: '借贷标识',
-                'x-decorator': 'FormItem',
-                'x-component': 'Select',
-                'x-validator': [],
+              '4bpk2z04f9i': {
+                type: 'void',
+                'x-component': 'Card',
                 'x-component-props': {
-                  allowClear: true,
-                  notFoundContent: '暂无数据',
-                  placeholder: '请选择数据',
+                  title: 'Title',
+                  style: { borderStyle: 'none' },
+                  bordered: false,
                 },
-                'x-decorator-props': { labelCol: 4, wrapperCol: 16 },
-                name: 'loanMark',
-                enum: [
-                  { children: [], label: '借-出金', value: '1' },
-                  { children: [], label: '贷-入金', value: '2' },
-                ],
-                'x-designable-id': 'clt3fp2ye34',
+                'x-reactions': {
+                  dependencies: [{ property: 'value', type: 'any' }],
+                  fulfill: {
+                    state: { componentProps: '{{{\n  title: ""\n}}}' },
+                  },
+                },
+                'x-designable-id': '4bpk2z04f9i',
+                properties: {
+                  '3eq0e64es18': {
+                    type: 'void',
+                    'x-component': 'FormGrid',
+                    'x-validator': [],
+                    'x-component-props': {},
+                    'x-designable-id': '3eq0e64es18',
+                    properties: {
+                      mutvsxlb6s9: {
+                        type: 'void',
+                        'x-component': 'FormGrid.GridColumn',
+                        'x-designable-id': 'mutvsxlb6s9',
+                        'x-index': 0,
+                      },
+                      kgg1t7h5m0p: {
+                        type: 'void',
+                        'x-component': 'FormGrid.GridColumn',
+                        'x-designable-id': 'kgg1t7h5m0p',
+                        'x-index': 1,
+                      },
+                      taaqejnu3dr: {
+                        type: 'void',
+                        'x-component': 'FormGrid.GridColumn',
+                        'x-validator': [],
+                        'x-component-props': {},
+                        'x-designable-id': 'taaqejnu3dr',
+                        properties: {
+                          company: {
+                            title: '',
+                            'x-decorator': 'FormItem',
+                            'x-component': 'Select',
+                            'x-validator': [],
+                            'x-component-props': {},
+                            'x-decorator-props': {
+                              wrapperAlign: 'right',
+                              labelAlign: 'left',
+                              feedbackLayout: 'loose',
+                              fullness: true,
+                              style: {
+                                display: 'flex',
+                                justifyContent: 'flex-end',
+                              },
+                            },
+                            enum: [
+                              { children: [], label: '北京公司', value: '1' },
+                              { children: [], label: '南京公司', value: '2' },
+                            ],
+                            name: 'company',
+                            'x-designable-id': 'kk6qdl9b79f',
+                            'x-index': 0,
+                          },
+                        },
+                        'x-index': 2,
+                      },
+                    },
+                    'x-index': 0,
+                  },
+                },
                 'x-index': 0,
               },
             },
+            'x-index': 0,
           },
         },
+        'x-index': 0,
       },
-      ba21ogalmqk: {
+      geci9nx45tg: {
         type: 'void',
         'x-component': 'FormGrid',
         'x-validator': [],
-        'x-component-props': {},
-        'x-designable-id': 'ba21ogalmqk',
+        'x-component-props': { columnGap: 0 },
+        'x-designable-id': 'geci9nx45tg',
         'x-index': 1,
         properties: {
-          e2uftqid1m1: {
-            type: 'void',
-            'x-component': 'FormGrid.GridColumn',
-            'x-designable-id': 'e2uftqid1m1',
-            'x-index': 0,
-          },
-          '7mti6cbm7h0': {
+          '8x13pmrg6qn': {
             type: 'void',
             'x-component': 'FormGrid.GridColumn',
             'x-validator': [],
-            'x-component-props': {},
-            'x-designable-id': '7mti6cbm7h0',
-            'x-index': 1,
-            properties: {
-              ljl4bm0bcur: {
-                type: 'void',
-                'x-component': 'Button',
-                'x-component-props': {
-                  text: '查询',
-                  type: 'primary',
-                  onClick:
-                    '{{() => {\n  $form.submit().then((values) => {\n    const {time,loadMark} = values;\n    const queryData = {};\n    if(time){\n      queryData.startTime = `${moment(time[0]).format("YYYY-MMM-DD hh:mm:ss")}`;\n      queryData.endTime = `${moment(time[1]).format("YYYY-MM-DD hh:mm:ss")}`;\n    }\n    console.log(queryData);\n  })\n}}}',
-                },
-                'x-designable-id': 'ljl4bm0bcur',
-                'x-index': 0,
+            'x-component-props': { gridSpan: 1 },
+            'x-reactions': {
+              dependencies: [{ property: 'value', type: 'any' }],
+              fulfill: {
+                state: { componentProps: "{{{\n  className:'col1'\n}}}" },
               },
-              ru4q0h8ptaq: {
+            },
+            'x-designable-id': '8x13pmrg6qn',
+            'x-index': 0,
+            properties: {
+              xpybfgyzn9g: {
                 type: 'void',
                 'x-component': 'Button',
-                'x-component-props': {
-                  text: '重置',
-                  onClick:
-                    '{{() => {\n  $form.setValues({\n    time: undefined,\n    loanMark: undefined\n  });\n}}}',
-                },
-                'x-designable-id': 'ru4q0h8ptaq',
-                'x-index': 1,
+                'x-component-props': { text: '第一列', type: 'primary' },
+                'x-designable-id': 'xpybfgyzn9g',
+                'x-index': 0,
               },
             },
           },
-          w4876r7oj9l: {
+          somo3aq3k6a: {
             type: 'void',
             'x-component': 'FormGrid.GridColumn',
-            'x-designable-id': 'w4876r7oj9l',
-            'x-index': 2,
             'x-validator': [],
-            'x-component-props': {},
+            'x-component-props': { style: { width: '330px' } },
+            'x-reactions': {
+              dependencies: [{ property: 'value', type: 'any' }],
+              fulfill: {
+                state: { componentProps: "{{{\n  className: 'col2'\n}}}" },
+              },
+            },
+            'x-designable-id': 'somo3aq3k6a',
+            'x-index': 1,
+            properties: {
+              bv5nuk1d3jx: {
+                type: 'void',
+                'x-component': 'Button',
+                'x-component-props': { text: '右侧列', type: 'primary' },
+                'x-designable-id': 'bv5nuk1d3jx',
+                'x-index': 0,
+              },
+            },
           },
         },
       },
     },
     designableType: 'form-root',
-    'x-designable-id': 'uhr0gsrnv62',
+    'x-designable-id': 'q0qp4c9us7z',
   },
 };
 
